8c628bf7f5990400afa69c173afaa417663a93be
Vladimir <dvvinfo@mail.ru>
2025-02-03 15:08:40 +0300

    added BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+  type?: 'danger' | 'cancel'
+}>()
+</script>
+
+<template>
+  <button :class="['base-button', type]">
+    <slot>{{ label }}</slot>
+  </button>
+</template>
+
+<style lang="scss">
+.base-button {
+  padding: 8px 15px;
+  border-radius: 8px;
+  border: none;
+  cursor: pointer;
+  font-size: 14px;
+  transition: 0.2s;
+  text-align: center;
+  line-height: 16.7px;
+  &:hover {
+    opacity: 0.8;
+  }
+  &.danger {
+    background-color: var(--vt-c-red);
+    color: var(--vt-c-white);
+  }
+
+  &.cancel {
+    background-color: var(--vt-c-white);
+    color: var(--vt-c-black-soft);
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue'
+</script>
 
 <template>
   <h1>Home</h1>
+  <BaseButton type="danger" label="Click me" />
 </template>
